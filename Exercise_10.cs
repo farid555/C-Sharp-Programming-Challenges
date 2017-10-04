bad1096a7b65b163692351136f2fcdd07d6d3763
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-04 14:49:59 -0500

    Create Exercise_10.cs

---
+++ Exercise_10.cs
@@ -0,0 +1,37 @@
+using System;
+					
+public class Exercise_10
+{
+	public static void Main()
+	{
+		
+		/******************************************************************** 
+		 *
+		 *	10. Write a program that takes three numbers(x,y,z)
+		 *	   as input and print the output of (x+y)*z and x*y + y*z.
+		 *
+		 *
+		 * 	By: Jesus Hilario Hernandez
+		 * 	Last Updated: October 4th 2017
+		 *
+		 ********************************************************************/
+		Console.WriteLine("Enter the number 5 as x: ");
+		var x = Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("Enter the number 6 as y: ");
+		var y = Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("Enter the number 7 as z:  ");
+		var z = Convert.ToInt32(Console.ReadLine());
+		
+		Console.Write("Result of specified numbers ");
+		Console.WriteLine("{0}, {1}, and {2}", x, y, z);
+		
+		Console.Write("as x, y ,z in (x + y) * z is ");
+		Console.WriteLine("{0} ", (x + y) * z);
+		
+		Console.Write("and in (x*y + y*z) is ");
+		Console.WriteLine("{0}.", (x*y + y*z));
+		
+	}
+}
