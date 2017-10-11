ba7ce63d610e0834ce6047b6295d1da010d0337e
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-11 09:58:45 -0500

    Create Exercise_20.cs

---
+++ Basic Exercises [53 excercises with solutions]/Exercise_20.cs
@@ -0,0 +1,41 @@
+/********************************************************************
+*
+*	19. Write a program to get the absolute value of the difference
+*	    between two given numbers. Return double the absolute value
+*		of the difference if the first number is greater than second
+*		number.
+*
+*
+* 	By: Jesus Hilario Hernandez
+* 	Last Updated: October 11th 2017
+*
+*
+********************************************************************/
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+		var num1 = 29; var num2 = 2;
+		if (num1 > num2)
+		{
+			return (num1 - num2) * 2;
+		}
+		else
+		{
+			
+		}
+		Console.WriteLine();
+		
+		
+		// W3resource Reply
+		
+	}
+	
+	//public static int result (int a, int b)
+	//{
+		
+	//}
+}
