df1a82e32d28d4d7963cd961140c0c44af4ce8fe
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-10-11 17:33:53 -0500

    added

--- Basic Exercises [53 excercises with solutions]/Exercise_20.cs
+++ Basic Exercises [53 excercises with solutions]/Exercise_20.cs
@@ -33,12 +33,19 @@ public class Exercise_20
 		}
 
 
-		// W3resource Reply
+		/**************************
+		 *	W3resource's Solution
+		 **************************/
 
+		Console.WriteLine(result(13, 40));
+		Console.WriteLine(result(50, 21));
+		Console.WriteLine(result(0, 23));
 	}
 
-	//public static int result (int a, int b)
-	//{
-
-	//}
+	public static int result (int a, int b)
+	{
+		if (a > b)
+			return (a - b) * 2;
+		return b - a;
+	}
 }
