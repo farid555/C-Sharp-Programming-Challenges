8c4ff4c17d10e26e1c9e76ca8e8a4777a83e65bd
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-10-14 08:29:29 -0500

    added

---
+++ Basic Exercises [53 excercises with solutions]/Exercise_33.cs
@@ -0,0 +1,38 @@
+/********************************************************************
+*
+*	33. Write a program to check if a given number is a multiple of 3
+*		or a multiple of 7.
+*
+* 	By: Jesus Hilario Hernandez
+* 	Last Updated: October 13th 2017
+*
+*
+********************************************************************/
+using System;
+
+
+public class Exercise_33
+{
+	public static void Main()
+	{
+		/************************
+		 *	Jesus' Solution
+		 ************************/
+		Console.WriteLine("Input your first integer: ");
+		var number = Convert.ToInt32(Console.ReadLine());
+		if (number % 3 == 0)
+		{
+
+		}
+
+
+		/*******************************************
+		 *	Jesus' Solution After checking response
+		 *******************************************/
+
+		/**************************
+		 *	W3resource's Solution
+		 **************************/
+
+	}
+}
