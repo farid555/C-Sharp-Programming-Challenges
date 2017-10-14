f26efcd96a6052cc335cc8ea7db4d3ad7856e689
jesushilarioh <jesushilariohernandez@gmail.com>
2017-10-14 11:29:37 -0500

    a program that checks if a number is a multiple of 3 or 7

--- Basic Exercises [53 excercises with solutions]/Exercise_33.cs
+++ Basic Exercises [53 excercises with solutions]/Exercise_33.cs
@@ -21,18 +21,26 @@ public class Exercise_33
 		Console.WriteLine("Input your first integer: ");
 		var number = Convert.ToInt32(Console.ReadLine());
 		if (number % 3 == 0)
-		{
-
-		}
+			Console.WriteLine(number % 3 == 0);
+		else
+			Console.WriteLine(number % 3 == 0);
 
 
 		/*******************************************
 		 *	Jesus' Solution After checking response
 		 *******************************************/
+		var cond = number % 3 == 0 || number % 7 == 0 || number > 0;
+		if (cond)
+			Console.WriteLine(cond);
 
 		/**************************
 		 *	W3resource's Solution
 		 **************************/
-
+		Console.WriteLine("\nInput first integer:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        if (x > 0)
+        {
+			Console.WriteLine(x % 3 == 0 || x % 7 == 0);
+        }
 	}
 }
