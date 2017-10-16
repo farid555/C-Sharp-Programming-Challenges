58b54f945efbf282dc954fcd3d9042dec61c4c1f
jesushilarioh <jesushilariohernandez@gmail.com>
2017-10-15 21:47:30 -0500

    added

---
+++ Basic Exercises [53 excercises with solutions]/Exercise_42.cs
@@ -0,0 +1,37 @@
+/********************************************************************
+*
+*	42. Write a C# program to create a new string where the first
+*	    4 characters will be in lower case. If the string is less
+*		than 4 characters then make the whole string in upper case.
+*
+*	  Test Data:
+*	  Input a string: w3r
+*	  Sample Output
+*	  W3R
+*
+* 	By: Jesus Hilario Hernandez
+* 	Last Updated: October 15th 2017
+*
+********************************************************************/
+using System;
+using System.Linq;
+
+public class Exercise_42
+{
+	public static void Main()
+	{
+		/************************
+		 *	Jesus' Solution
+		 ************************/
+		 Console.Write("Input a string: ");
+		 var string1 = Convert.ToString(Console.ReadLine());
+		/*******************************************
+		 *	Jesus' Solution After checking response
+		 *******************************************/
+
+		/**************************
+		 *	W3resource's Solution
+		 **************************/
+
+	}
+}
