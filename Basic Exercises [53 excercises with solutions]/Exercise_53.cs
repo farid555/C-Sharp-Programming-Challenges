9cb4865722ed58895d4e7a820723f6a838b1d805
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-24 13:44:18 -0500

    Update Exercise_53.cs

--- Basic Exercises [53 excercises with solutions]/Exercise_53.cs
+++ Basic Exercises [53 excercises with solutions]/Exercise_53.cs
@@ -21,14 +21,37 @@ public class Exercise_53
 		 *	Jesus' Solution
 		 ************************/
 		var Array1 = new int[] {2, 4, 7, 8, 6};
-		Console.WriteLine("\nOriginal array: [{1}]", string.Join(Array1));
+		Console.WriteLine("\nOriginal array: [{0}]", string.Join(", ", Array1));
 		 /*******************************************
  		 *	Jesus' Solution After checking response
  		 *******************************************/
+		Console.WriteLine("Check if an array has on odd number?" + checkIfOdd(Array1));
 
 		/**************************
 		 *	W3resource's Solution
 		 **************************/
-
+		int [] nums = {2, 4, 7, 8, 6};
+		Console.WriteLine("\nOriginal array: [{0}]", string.Join(", ", nums));
+		Console.WriteLine("Check if an array contains an odd number? " + even_odd(nums));
+	}
+	
+	public static bool even_odd(int[] nums)
+	{
+		foreach (var n in nums)
+		{
+			if(n % 2 != 0)
+				return true;
+		}
+		return false;
+	}
+	/********************************************
+ 	 *	Jesus' Methods After checking response	*
+ 	 ********************************************/
+	public static bool checkIfOdd(int[] arrayNums)
+	{
+		foreach(var n in arrayNums)
+			if (n % 2 != 0)
+				return true;
+		return false;
 	}
 }
