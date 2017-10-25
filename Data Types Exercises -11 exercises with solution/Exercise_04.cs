3cfd27ba61afa911487df36f48f9f7cc6df1252e
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-25 14:45:44 -0500

    Update Exercise_04.cs

--- Data Types Exercises -11 exercises with solution/Exercise_04.cs
+++ Data Types Exercises -11 exercises with solution/Exercise_04.cs
@@ -1,20 +1,15 @@
 /********************************************************************
 *
-*	04. Write a C# Sharp program that takes a number and a width also 
-*	    a number, as input and then displays a triangle of that width, 
-*	    using that number.
+*	04. Write a C# Sharp program that takes two numbers as input and 
+*		perform an operation (+,-,*,x,/) on them and
+*		displays the result of that operation.
 *
-*           Test Data
-*		Enter a number: 6 
-*		Enter the desired width: 6 
+*		Test Data
+*		Input first number: 20
+*		Input operation: - 
+*	 	Input second number: 12
 *		Expected Output :
-*
-*		666666                                                      
-*		66666                                                           
-*		6666                                                                  
-*		666                                                        
-*		66                                                                  
-*		6 
+*		20 - 12 = 8
 *
 * 	By: Jesus Hilario Hernandez
 * 	Last Updated: October 25th 2017
@@ -30,14 +25,46 @@ public class Exercise_04
 		/************************
 		 *	Jesus' Solution
 		 ************************/
-
+		Console.WriteLine("Input first #: ");
+		var firstNum = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Input operation: ");
+		var operation = Convert.ToChar(Console.ReadLine());
+		Console.WriteLine("Input second #: ");
+		var secondNum = Convert.ToInt32(Console.ReadLine());
+		// Console.WriteLine("{0} {1} {2} = {3}", firstNum, operation, secondNum, (Convert.ToString(firstNum + operation + secondNum)));
+		
 		/*******************************************
 		 *	Jesus' Solution After checking response
 		 *******************************************/
-
+		switch (Convert.ToString(operation))
+		{
+			case "-": 
+				Console.WriteLine("Hello");
+				break;
+			
+		}
 		/**************************
 		 *	W3resource's Solution
 		 **************************/
-
+		int x, y;
+        char operation;
+         
+        Console.Write("Input first number: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input operation: ");
+        operation = Convert.ToChar(Console.ReadLine());
+        Console.Write("Input second number: ");
+        y = Convert.ToInt32(Console.ReadLine());
+         
+        if (operation=='+')
+            Console.WriteLine("{0} + {1} = {2}", x, y, x+y);
+        else if (operation=='-')
+            Console.WriteLine("{0} - {1} = {2}", x, y, x-y);
+        else if ((operation=='x') || (operation=='*'))
+            Console.WriteLine("{0} * {1} = {2}", x, y, x*y);
+        else if (operation=='/')
+            Console.WriteLine("{0} / {1} = {2}", x, y, x/y);
+        else
+            Console.WriteLine("Wrong Character");
 	}
 }
