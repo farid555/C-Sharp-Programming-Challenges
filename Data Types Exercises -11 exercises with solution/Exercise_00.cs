af154d302a40a875dad2b32f488f1c2581c69599
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-25 10:09:47 -0500

    skeleton

--- Data Types Exercises -11 exercises with solution/Exercise_00.cs
+++ Data Types Exercises -11 exercises with solution/Exercise_00.cs
@@ -1,6 +1,6 @@
 /********************************************************************
 *
-*	00. Write a program 
+*		00. Write a program 
 *
 * 	By: Jesus Hilario Hernandez
 * 	Last Updated: October 25th 2017
@@ -8,7 +8,6 @@
 *
 ********************************************************************/
 using System;
-
 
 public class Exercise_00
 {
