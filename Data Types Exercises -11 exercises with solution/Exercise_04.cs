1b8341bc0739459b265e446c5db829533712bd82
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-25 13:53:41 -0500

    Create Exercise_04.cs

---
+++ Data Types Exercises -11 exercises with solution/Exercise_04.cs
@@ -0,0 +1,43 @@
+/********************************************************************
+*
+*	02. Write a C# Sharp program that takes a number and a width also 
+*		a number, as input and then displays a triangle of that width, 
+*		using that number.
+*
+*		Test Data
+*		Enter a number: 6 
+*		Enter the desired width: 6 
+*		Expected Output :
+*
+*		666666                                                      
+*		66666                                                           
+*		6666                                                                  
+*		666                                                        
+*		66                                                                  
+*		6 
+*
+* 	By: Jesus Hilario Hernandez
+* 	Last Updated: October 25th 2017
+*
+*
+********************************************************************/
+using System;
+
+public class Exercise_02
+{
+	public static void Main()
+	{
+		/************************
+		 *	Jesus' Solution
+		 ************************/
+
+		/*******************************************
+		 *	Jesus' Solution After checking response
+		 *******************************************/
+
+		/**************************
+		 *	W3resource's Solution
+		 **************************/
+
+	}
+}
