5e609180a4c970b0593df70042a79a77bbfffe63
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-26 13:57:15 -0500

    Update Exercise_05.cs

--- Data Types Exercises -11 exercises with solution/Exercise_05.cs
+++ Data Types Exercises -11 exercises with solution/Exercise_05.cs
@@ -1,14 +1,9 @@
 /********************************************************************
 *
-*	05. Write a C# Sharp program that takes the radius of a circle as 
-*		input and calculate the perimeter and 
-*		area of the circle. 
+*	05. Write a C# Sharp program to display certain values of the 
+*		function x = y2 + 2y + 1 (using integer numbers for y , 
+*		ranging from -5 to +5).
 *
-*		Test Data
-*		Input the radius of the circle :
-*		12 
-*		Expected Output :
-*		Perimeter of Circle : 75.36
 *
 * 	By: Jesus Hilario Hernandez
 * 	Last Updated: October 26th 2017
@@ -36,11 +31,12 @@ public class Exercise_05
 		 *	W3resource's Solution
 		 **************************/
 		double r,per_cir;
-    double PI = 3.14;
-    Console.WriteLine("Input the radius of the circle : ");
-    r = Convert.ToDouble(Console.ReadLine());
-    per_cir = 2 * PI * r;
-    Console.WriteLine("Perimeter of Circle : {0}", per_cir);
-    Console.Read();
+        double PI = 3.14;
+        Console.WriteLine("Input the radius of the circle : ");
+        r = Convert.ToDouble(Console.ReadLine());
+        per_cir = 2 * PI * r;
+        Console.WriteLine("Perimeter of Circle : {0}", per_cir);
+        //Console.ReadLine();
+		
 	}
 }
