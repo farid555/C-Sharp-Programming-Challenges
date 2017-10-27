53eda7ce66be7906994166cb7b6f21218a27d7e5
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-10-27 11:02:41 -0500

    Update Exercise_06.cs

--- Data Types Exercises [11 exercises with solutions]/Exercise_06.cs
+++ Data Types Exercises [11 exercises with solutions]/Exercise_06.cs
@@ -1,8 +1,8 @@
 /********************************************************************
 *
 *	06. Write a C# Sharp program to display certain values of the 
-*		function x = y2 + 2y + 1 (using integer numbers for y , 
-*		ranging from -5 to +5).
+*	    function x = y2 + 2y + 1 (using integer numbers for y , 
+*	    ranging from -5 to +5).
 *
 *
 * 	By: Jesus Hilario Hernandez
@@ -19,25 +19,32 @@ public class Exercise_06
 		/************************
 		 *	Jesus' Solution
 		 ************************/
-		Console.WriteLine(equation(9));
+		for (var i = -5; i <= 5; i++)
+		{
+			var x2 = (i * i) - (2 * i) + 1;
+			// Console.Write("y = {0}; ", i);
+			Console.WriteLine("x = {0}\xB2  + 2({0}) + 1 = {1}; While y = {0}", i, x2);
+			
+		}
+		
 		/*******************************************
 		 *	Jesus' Solution After checking response
 		 *******************************************/
-
+		Console.WriteLine();
+		
 		/**************************
 		 *	W3resource's Solution
 		 **************************/
-	
-	}
-	public static int equation(int y)
-	{
-		int x = 9;
-		for (var i = -5; i <= 5; i++)
-		{
-			x = (y * y) + (2 * y) + 1;
-			Console.WriteLine(x);
-			x++;
-		}
-	return x;	
+		int x, y;
+         
+        Console.WriteLine("x = y² - 2y +1");
+        Console.WriteLine();
+		
+       	for (y=-5; y <= 5; y++)
+        {
+			x = y*y - 2*y + 1;
+            Console.WriteLine(
+            "y = {0} ; x=({0})² - 2*({0}) +1 = {1}", y, x);
+        }
 	}
 }
