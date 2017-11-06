498a224aaa0f2c8488380dc0bf89a59ff9c2507e
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-11-06 13:38:44 -0600

    Update Exercise_06.cs

--- Conditional Statement [25 exercises with solution]/Exercise_06.cs
+++ Conditional Statement [25 exercises with solution]/Exercise_06.cs
@@ -1,6 +1,6 @@
 /********************************************************************
 *
-*	05. Write a C# Sharp program to read the value of an integer m 
+*	06. Write a C# Sharp program to read the value of an integer m 
 *		and display the value of n is 1 when m is larger than 0, 
 *		0 when m is 0 and -1 when m is less than 0.
 *
@@ -15,7 +15,7 @@
 ********************************************************************/
 using System;
 
-public class Exercise_05
+public class Exercise_06
 {
 	public static void Main()
 	{
