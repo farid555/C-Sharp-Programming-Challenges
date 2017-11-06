5892dece2b64ecc6e5e3e392db18a07f2c4e664a
Jesus Hilario H <jesushilarioh@users.noreply.github.com>
2017-11-06 12:26:58 -0600

    Update Exercise_05.cs

--- Conditional Statement [25 exercises with solution]/Exercise_05.cs
+++ Conditional Statement [25 exercises with solution]/Exercise_05.cs
@@ -34,6 +34,22 @@ public class Exercise_05
 		/**************************
 		 *	W3resource's Solution
 		 **************************/
+		int vote_age;
+    	Console.Write("\n\n");
+    	Console.Write("Detrermine a specific age is eligible for casting the vote:\n");
+    	Console.Write("----------------------------------------------------------");
+    	Console.Write("\n\n");
 
+
+    	Console.Write("Input the age of the candidate : ");
+    	vote_age= Convert.ToInt32(Console.ReadLine());
+  		if (vote_age<18)
+     	{
+       		Console.Write("Sorry, You are not eligible to caste your vote.\n");
+       		Console.Write("You would be able to caste your vote after {0} year.\n\n",18-vote_age);
+     	}
+  		else
+     		Console.Write("Congratulation! You are eligible for casting your vote.\n\n");
+		
 	}
 }
