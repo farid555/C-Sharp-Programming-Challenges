b4c98cd8eb5f9f8049684e9000b89d551ea5e7d4
jesushilarioh <jesushilariohernandez@gmail.com>
2017-11-19 16:31:11 -0600

    added

---
+++ Conditional Statement [25 exercises with solution]/Exercise_09_Check.cs
@@ -0,0 +1,37 @@
+/********************************************************************
+*
+*	09. Write a C# program to accept a coordinate point in an XY
+*		coordinate system and determine in which quadrant the coordinate
+*		point lies.
+*
+*		Test Data:
+*		Input the value for x:
+*		Input the value for y:
+*		Expected Output:
+*		The coordinate point (7, 9) lies in the First Quadrant.
+*
+* 	By: Jesus Hilario Hernandez
+* 	Last Updated: November 19th, 2017
+*
+*
+********************************************************************/
+using System;
+
+public class Exercise_09
+{
+	public static void Main()
+	{
+		/************************
+		 *	Jesus' Solution
+		 ************************/
+
+		/*******************************************
+		 *	Jesus' Solution After checking response
+		 *******************************************/
+
+		/**************************
+		 *	W3resource's Solution
+		 **************************/
+
+	}
+}
