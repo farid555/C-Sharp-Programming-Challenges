e01e61c89048ec92065f60a0ab36ca67053173aa
jesushilarioh <jesushilariohernandez@gmail.com>
2017-12-05 14:36:45 -0600

    added Dec. 5th 2017

--- Conditional Statement [25 exercises with solution]/Exercise_12.cs
+++ Conditional Statement [25 exercises with solution]/Exercise_12.cs
@@ -55,7 +55,7 @@ public class Exercise_12
 
 		Console.WriteLine("Total Marks = {0}", numbers.Sum());
 		Console.WriteLine("Percentage = {0}", Math.Round(numbers.Average(), 1));
-		Console.WriteLine("Number of grades is: {0}", numbers.Count());
+		Console.WriteLine("Number of grades: {0}", numbers.Count());
 
 		/*******************************************
 		 *	Jesus' Solution After checking response
@@ -64,6 +64,45 @@ public class Exercise_12
 		/**************************
 		 *	W3resource's Solution
 		 **************************/
+		double rl,phy,che,ca,total;
+		double per;
+		string nm,div;
+
+		Console.Write("\n\n");
+		Console.Write("Calculate the total, percentage and division to take marks of three subjects:\n");
+		Console.Write("-------------------------------------------------------------------------------");
+		Console.Write("\n\n");
+
+
+		Console.Write("Input the Roll Number of the student :");
+		rl = Convert.ToInt32(Console.ReadLine());
+
+		Console.Write("Input the Name of the Student :");
+		nm = Console.ReadLine();
+
+		Console.Write("Input  the marks of Physics : ");
+		phy= Convert.ToInt32(Console.ReadLine());
+		Console.Write("Input  the marks of  Chemistry : ");
+		che = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Input  the marks of Computer Application : ");
+		ca = Convert.ToInt32(Console.ReadLine());
+
+		total = phy+che+ca;
+		per = total/3.0;
+		if (per>=60)
+		 	div="First";
+		else
+		if (per<60&&per>=48)
+		    div="Second";
+		else
+		    if (per<48&&per>=36)
+			div="Pass";
+		     else
+			div="Fail";
+
+        Console.Write("\nRoll No : {0}\nName of Student : {1}\n",rl,nm);
+        Console.Write("Marks in Physics : {0}\nMarks in Chemistry : {1}\nMarks in Computer Application : {2}\n",phy,che,ca);
+        Console.Write("Total Marks = {0}\nPercentage = {1}\nDivision = {2}\n",total,per,div);
 
 	}
 }
