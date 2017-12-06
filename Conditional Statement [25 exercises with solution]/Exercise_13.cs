a9106a6a2773e47cc990e8f0f64f6935f4a6a58b
jesushilarioh <jesushilariohernandez@gmail.com>
2017-12-06 13:47:36 -0600

    added Dec. 6th 2017

---
+++ Conditional Statement [25 exercises with solution]/Exercise_13.cs
@@ -0,0 +1,41 @@
+/********************************************************************
+*
+*	13. Write a C# Sharp program to read temperature in centigrade and
+*		display a suitable message according to temperature state below
+*
+*		Temp < 0 then Freezing weather
+*		Temp 0-10 then Very Cold weather
+*		Temp 10-20 then Cold weather
+*		Temp 20-30 then Normal in Temp
+*		Temp 30-40 then Its Hot
+*		Temp >=40 then Its Very Hot
+*		Test Data :
+*		42
+*		Expected Output :
+*		Its very hot.
+*
+* 	By: Jesus Hilario Hernandez
+* 	Last Updated: October 25th 2017
+*
+*
+********************************************************************/
+using System;
+
+public class Exercise_13
+{
+	public static void Main()
+	{
+		/************************
+		 *	Jesus' Solution
+		 ************************/
+
+		/*******************************************
+		 *	Jesus' Solution After checking response
+		 *******************************************/
+
+		/**************************
+		 *	W3resource's Solution
+		 **************************/
+
+	}
+}
