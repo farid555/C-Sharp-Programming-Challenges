7d0f8755f99dbf18bd566991eb8f4d3625ac74d5
jesushilarioh <jesushilariohernandez@gmail.com>
2018-02-02 19:33:00 -0600

    added

--- w3resource.com/Conditional Statement [25 exercises with solution]/Exercise_20.cs
+++ w3resource.com/Conditional Statement [25 exercises with solution]/Exercise_20.cs
@@ -22,7 +22,29 @@ public class Exercise_20
 		/************************
 		 *	Jesus' Solution
 		 ************************/
-
+		switch (day_number) {
+			case 1:
+				Console.WriteLine("Monday");
+				break;
+			case 2:
+				Console.WriteLine("Tuesday");
+				break;
+			case 3:
+				Console.WriteLine("Wednesday");
+				break;
+			case 4:
+				Console.WriteLine("Thursday");
+				break;
+			case 5:
+				Console.WriteLine("Friday");
+				break;
+			case 6:
+				Console.WriteLine("Saturday");
+				break;
+			case 7:
+				Console.WriteLine("Sunday");
+				break;
+		}
 		/*******************************************
 		 *	Jesus' Solution After checking response
 		 *******************************************/
